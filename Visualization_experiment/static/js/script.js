2dd21f16b1024dda30e0ef3e4c93311abbf06be3
SnewZz <64595565+SnewZz@users.noreply.github.com>
2023-05-04 16:41:56 +0200

    Improve of the view

--- Visualization_experiment/static/js/script.js
+++ Visualization_experiment/static/js/script.js
@@ -18,12 +18,16 @@ function getColor(n) {
     }
 }
 
+function updateDataInfo(e){
+    document.getElementById('data-info').innerText = e;
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     /**
      * Open and manipulate the csv file
      */
-    d3.csv("../data/au.csv").then(function (data) {
+    d3.csv("../data/coordCities.csv").then(function (data) {
         // Create Leaflet map
         var map = L.map('map').setView([-25, 135], 4);
 
@@ -36,15 +40,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var cities = [];
 
-        var populationTotale = 0;
+        //var populationTotale = 0;
         data.forEach(function (d) {
             cities.push({
+                id: parseInt(d.id),
                 name: d.city,
                 lat: parseInt(d.lat),
-                lng: parseInt(d.lng),
-                pop: parseInt(d.population)
+                lng: parseInt(d.long),
             });
-            populationTotale += parseInt(d.population);
+            //populationTotale += parseInt(d.population);
         });
         var geoJSONLayer;
         $.getJSON("../static/geoJSON/local-government-area.geojson", function (data) {
@@ -76,93 +80,15 @@ document.addEventListener("DOMContentLoaded", function () {
                         weight: 2,
                         color: 'white'
                     }
-                }).addTo(map);
-            }
-            geoJSONLayer.eachLayer(function(layer){
-                layer.on('click', function(){
-                    var regionId = layer.feature.id;
-                    var regionName = layer.feature.properties.lga_name_long;
-                    updateDataInfo(regionName)
+                }).addTo(map).eachLayer(function(layer){
+                    layer.on('click', function(){
+                        var regionId = layer.feature    ;
+                        console.log(regionId)
+                        var regionName = layer.feature.properties.lga_name_long;
+                        updateDataInfo(regionName)
+                    });
                 })
-            })
+            }
         });
-
-        // Add markers with data to the map
-        // var marker1 = L.marker([-36.0737, 146.9135]).addTo(map).bindPopup("Albury");
-        // var marker2 = L.marker([-34.9285, 138.6007]).addTo(map).bindPopup("Adelaide");
-        // var marker3 = L.marker([-35.0031, 117.8657]).addTo(map).bindPopup("Albany");
-        // var marker4 = L.marker([-23.6980, 133.8807]).addTo(map).bindPopup("Alice Springs");
-
-        // // Attach click event listener to markers
-        // marker1.on('click', function () {
-        //     updateDataInfo("Data for Marker 1");
-        //     generateChart();
-        // });
-
-        // marker2.on('click', function () {
-        //     updateDataInfo("Data for Marker 2");
-        //     generateChart();
-        // });
-
-        // marker3.on('click', function () {
-        //     updateDataInfo("Data for Marker 3");
-        //     generateChart();
-        // });
-
-        // marker4.on('click', function () {
-        //     updateDataInfo("Data for Marker 4");
-        //     generateChart();
-        // });
-
-        // Update data info in the dashboard
-        // function updateDataInfo(data) {
-        //     document.getElementById('data-info').innerText = data;
-        // }
-
-        // // Generate random temperature data for the chart
-        // function generateRandomTempData() {
-        //     var tempData = [];
-        //     for (var i = 0; i < 7; i++) { // Generating data for 7 months
-        //         tempData.push(Math.floor(Math.random() * 20) + 10); // Random temperature between 10 and 30
-        //     }
-        //     return tempData;
-        // }
-        // var chart;
-        // // Generate chart
-        // function generateChart() {
-        //     var ctx = document.getElementById('chart').getContext('2d');
-        //     var tempData = generateRandomTempData();
-        //     // Détruire l'instance précédente du graphique s'il en existe une
-        //     if (typeof chart !== 'undefined') {
-        //         chart.destroy();
-        //     }
-        //     chart = new Chart(ctx, {
-        //         type: 'line',
-        //         data: {
-        //             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        //             datasets: [{
-        //                 label: 'Temperature',
-        //                 data: tempData,
-        //                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        //                 borderColor: 'rgba(75, 192, 192, 1)',
-        //                 borderWidth: 1
-        //             }]
-        //         },
-        //         options: {
-        //             responsive: true,
-        //             maintainAspectRatio: false,
-        //             scales: {
-        //                 y: {
-        //                     beginAtZero: true,
-        //                     max: 40 // Set the maximum value on y-axis to 40
-        //                 }
-        //             }
-        //         }
-        //     });
-        //     var chartContainer = document.getElementById('chart').parentNode;
-        //     chartContainer.style.width = chart.width + "px";
-        //     chartContainer.style.height = chart.height + "px";
-
-        // }
     });
 });
