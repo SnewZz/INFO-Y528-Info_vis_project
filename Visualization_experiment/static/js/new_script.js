ce0d0f7278577a31327aa8c15ed2331008f2c547
Maxime1299 <57593135+Maxime1299@users.noreply.github.com>
2023-05-14 21:04:24 +0200

    add boostrap

--- Visualization_experiment/static/js/new_script.js
+++ Visualization_experiment/static/js/new_script.js
@@ -4,6 +4,11 @@
  * @param {number} n - The number of city in a region for which we want the color
  * @returns {string} The color, if the number of city is close to 40 it will be darker blue, else lighter blue. If the number is 0 the color is white.
  */
+
+var cities = [];
+var citiesPerRegion = [];
+var regions = [];
+
 function getColor(n) {
     if (n === 0) {
         return '#F0F0F0';
@@ -21,7 +26,7 @@ function updateDataInfo(e) {
 }
 
 function loadCities() {
-    var cities = []
+    //var cities = []
 
     d3.csv("../static/data/coord_cities.csv").then(function (data) {
         data.forEach(function (d) {
@@ -120,9 +125,37 @@ function getTemperatureYear(year) {
         console.log("ok")
         console.log(data[0].End_date.split("-")[0] === year)
         filteredData = data.filter(function (d) { return d.End_date.split("-")[0] === year.toString(); })
-
         return filteredData
     }))
+    //return filteredData;
+}
+
+function getHotestCities(year,season) {
+    d3.csv("../static/data/Datamap_1.csv").then(function (data) {
+        var best_cities = [];
+        var best_temp = [];
+        for (let i = 0; i < cities.length; i++){
+            var temperatures = data.filter(function (d) { return d.Season === season && d.city === cities[i].name && d.Start_date.split("-")[0] === year.toString()});
+            if (best_cities.length < 3){
+                best_cities.push(temperatures[0].city);
+                best_temp.push(parseFloat(temperatures[0].TempMax));
+            }
+            else{
+                //console.log("Yes")
+                console.log(Math.min(best_temp))
+                //console.log(Math.min(parseFloat(best_temp)));// < temperatures[0].TempMax)
+                if (Math.min(best_temp) < temperatures[0].TempMax){
+                    //console.log("Yes");
+                    var index = best_temp.indexOf(Math.min(best_temp));
+                    best_cities[index] = temperatures[0].city;
+                    best_temp[index] = temperatures[0].TempMax;
+                    //console.log(best_cities[0])
+                }
+            }
+        }   
+        console.log(best_cities);
+        return best_cities
+    })
     //return filteredData;
 }
 
@@ -134,6 +167,7 @@ function avgOnList(list){
     var avg = totalValue/list.length;
     return avg;
 }
+
 
 function getTemperatureAverageYear(data, citiesPerRegion) {
     var avgTemperatureRegion = [];
@@ -156,49 +190,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // var cities = loadCities();
-var citiesPerRegion = loadCitiesPerRegion(loadCities());
-var regions = loadRegions();
 
-
-// $.getJSON("../static/geoJSON/local-government-area.geojson", function (data) {
-
-//     var regions = {};
-//     var regionsColor = {};
-//     for (var i = 0; i < data.features.length; i++) {
-//         regions[i] = [];
-//         regionsColor[i] = 0;
-//         var lga = data.features[i];
-//         for (var j = 0; j < cities.length; j++) {
-//             var city = cities[j];
-//             var point = turf.point([city.lng, city.lat]);
-//             if (turf.booleanPointInPolygon(point, lga)) {
-//                 regions[i].push(city);
-//                 regionsColor[i]++;
-//             }
-//         }
-//     }
-
-//     // Parcourir à nouveau le GeoJSON et ajouter chaque région à la carte en lui attribuant une couleur 
-//     for (var i = 0; i < data.features.length; i++) {
-//         var lga = data.features[i];
-//         var fillColor = getColor(regionsColor[i])
-//         L.geoJSON(lga, {
-//             style: {
-//                 fillColor: fillColor,
-//                 fillOpacity: 0.8,
-//                 weight: 2,
-//                 color: 'white'
-//             }
-//         }).addTo(map).eachLayer(function (layer) {
-//             layer.on('click', function () {
-//                 var regionId = layer.feature;
-//                 console.log(regionId)
-//                 var regionName = layer.feature.properties.lga_name_long;
-//                 updateDataInfo(regionName)
-//             });
-//         })
-//     }
-// });
 
 
 const slider1 = document.getElementById("slider1");
@@ -207,6 +199,52 @@ const slider2 = document.getElementById("slider2");
 const result1 = document.getElementById("result1");
 const result2 = document.getElementById("result2");
 
+const radio_btn_1= document.getElementById("r1");
+const radio_btn_2= document.getElementById("r2");
+const radio_btn_3= document.getElementById("r3");
+const radio_btn_4= document.getElementById("r4");
+
+const year_select=document.getElementById("Year");
+const season_select=document.getElementById("Season");
+
+year_select.addEventListener('change',event=>{
+    console.log(event.target.value);
+    document.getElementById("selected_year").innerHTML = event.target.value;
+});
+
+season_select.addEventListener('change',event=>{
+    console.log(event.target.value);
+    document.getElementById("selected_season").innerHTML = event.target.value;
+});
+
+//Function that initialise everything
+window.addEventListener('load',function(){
+    document.getElementById("selected_year").innerHTML = this.document.getElementById("Year").value;
+    document.getElementById("selected_season").innerHTML = this.document.getElementById("Season").value;
+    //Chercher les villes les plus chaudes en 2009
+    loadCities();
+    loadCitiesPerRegion(cities);
+    loadRegions();
+    getHotestCities(2010,"Summer");
+    
+});
+
+radio_btn_1.addEventListener('change', () => {
+    console.log('Bouton radio sélectionné:', "Display min temp");
+  });
+  
+radio_btn_2.addEventListener('change', () => {
+    console.log('Bouton radio sélectionné:', "Display max temp");
+  });
+
+radio_btn_3.addEventListener('change', () => {
+    console.log('Bouton radio sélectionné:', "Display rainfall");
+    });
+
+radio_btn_4.addEventListener('change', () => {
+    console.log('Bouton radio sélectionné:', "Display sunshine");
+    });
+  
 slider1.addEventListener("change", async (event) => {
 
     data = await getTemperatureYear(event.target.value);
@@ -275,3 +313,4 @@ slider2.addEventListener("change", async (event) => {
         });
     }
 })
+
